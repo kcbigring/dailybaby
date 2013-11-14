a7e6eb060afb995989f0d6f09cd303c7eeadb56a
kcbigring <kcbigring@gmail.com>
2013-11-14 10:26:42 -0700

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,67 @@
+set :stages, %w(production staging)
+set :default_stage, "production"
+require 'capistrano/ext/multistage'
+require 'bundler/capistrano'
+
+# set :whenever_command, "bundle exec whenever"
+# set :whenever_environment, defer { stage }
+# require 'whenever/capistrano'
+
+set :application, 'dailybaby'
+
+# RVM bootstrap
+require 'rvm/capistrano'
+set :rvm_ruby_string, :local              # use the same ruby as used locally for deployment
+set :rvm_autolibs_flag, "read-only"       # more info: rvm help autolibs
+set :rvm_install_with_sudo, true
+set :rvm_type, :system
+before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset
+before 'deploy:setup', 'rvm:create_gemset' # only create gemset
+
+# server details
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+# set :deploy_via, :remote_cache
+set :user, "passenger"
+set :use_sudo, false
+
+# repo details
+set :scm, :git
+#set :scm_username, "?"
+set :repository, "git@github.com:kcbigring/dailybaby.git"
+set :branch, "master"
+set :git_enable_submodules, 1
+
+# asset handling
+load 'deploy/assets'
+
+# tasks
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :link_db do
+    run "ln -s #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+  end
+  
+end
+
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+end
+
+before 'deploy:assets:precompile', 'deploy:link_db'
+after 'deploy:update_code', 'rvm:trust_rvmrc', 'deploy:migrate'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :rails_env, "production"
-set :deploy_to, "/var/www/production"
+set :deploy_to, "/var/www/dailybaby"
 
-role :web, "dailybaby.production"
-role :app, "dailybaby.production"
-role :db,  "dailybaby.production", :primary => true
+role :web, "dailybaby.web"
+role :app, "dailybaby.web"
+role :db,  "dailybaby.web", :primary => true
 
 set :rvm_bin_path, "/usr/local/rvm/bin"
