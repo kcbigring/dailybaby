19bc6ae5de1187ea6398faeacc029313bedbe8bc
kcbigring <kcbigring@gmail.com>
2013-11-25 20:13:33 -0700

    page size

--- app/controllers/scheduled_emails_controller.rb
+++ app/controllers/scheduled_emails_controller.rb
@@ -1,7 +1,7 @@
 class ScheduledEmailsController < ApplicationController
   
   def index
-    @scheduled_emails = ScheduledEmail.paginate(:page => params[:page], :per_page => 1).order(:created_at => :desc)
+    @scheduled_emails = ScheduledEmail.paginate(:page => params[:page], :per_page => 20).order(:created_at => :desc)
   end
 
 end
