913ae476d136b8d1bec17f087cdc6ac3caf135ee
Ryan Cook <cookrn@gmail.com>
2014-01-06 15:57:44 -0700

    If an email client supports HTML, then it should render a medium sized image inline in the email.
    * add the faker gem in dev/test
    * don't leave FDs laying around from IO
    * email is full HTML document
    * have text version of email as well
    * silence I18n deprecation warnings
    * grab original _and_ medium urls from SmugMug
    * add test to email rendering and delivery

--- lib/tasks/dailybaby.rake
+++ lib/tasks/dailybaby.rake
@@ -8,7 +8,21 @@ namespace :dailybaby do
       if parent
         kid = Kid.find(email.kid_id)
         if kid
-          image_url = SmugMugWrapper.new.get_image_url(email.image_id, email.image_key, kid.album.password) rescue nil
+          begin
+            image =
+              SmugMugWrapper.new.client.find_image \
+                email.image_id,
+                email.image_key
+
+            image_url =
+              {
+                :medium   => image.mediumurl,
+                :original => image.originalurl
+              }
+          rescue
+            image_url = nil
+          end
+
           parent.deliver(kid, image_url, email.caption) rescue nil
         end
       end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'successfully delivers daily mail' do
+    user    = User.new :email => Faker::Internet.email
+    parent  = Parent.new
+    caption = 'Yay!'
+    album   = Album.new :custom_url => 'https://google.com'
+
+    kid =
+      Kid.new \
+        :album     => album,
+        :birthdate => DateTime.parse( '03/01/2013' ).to_date,
+        :name      => 'Sean'
+
+    image_url =
+      {
+        :medium   => 'http://pictures.thedailybaby.com/Porter/i-BCHvVM3/0/M/pats-M.jpg',
+        :original => 'http://pictures.thedailybaby.com/Porter/i-BCHvVM3/0/O/pats.jpg'
+      }
+
+    mailer =
+      klass.daily_mail \
+        user,
+        parent,
+        kid,
+        image_url,
+        caption
+
+    before_count = ActionMailer::Base.deliveries.size
+    mailer.deliver
+    delivery = ActionMailer::Base.deliveries.last
+    after_count = ActionMailer::Base.deliveries.size
+
+    assert_operator \
+      after_count,
+      :>,
+      before_count
+
+    refute_empty delivery.attachments
+  end
+
+  private
+
+  def klass
+    UserMailer
+  end
 end
