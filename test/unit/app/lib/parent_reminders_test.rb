a74bd3f7f02f48b42918a8e0cec870bf6b2b3c91
Ryan Cook <cookrn@gmail.com>
2014-01-16 21:21:01 -0700

    add additional test about parents who prefer reminders re: parent upload reminders

--- test/unit/app/lib/parent_reminders_test.rb
+++ test/unit/app/lib/parent_reminders_test.rb
@@ -29,6 +29,24 @@ class ParentRemindersTest < ActiveSupport::TestCase
       @p2.id
   end
 
+  test 'fetches parents who prefer reminders' do
+    p3 = Parent.create
+
+    parents = @r.parents_who_prefer_reminders
+
+    assert_includes \
+      parents,
+      @p
+
+    assert_includes \
+      parents,
+      @p2
+
+    refute_includes \
+      parents,
+      p3
+  end
+
   test 'fetches parents who HAVE NOT ALREADY submitted a photo' do
     parents = @r.parents_without_scheduled_email
 
